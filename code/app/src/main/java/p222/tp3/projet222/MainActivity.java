274675d537503531881ba46d4be3c9cb692d3395
Marin <marin.ffxiv@gmail.com>
2020-03-09 09:28:46 +0100

    add getData method to DatabaseHelper

--- code/app/src/main/java/p222/tp3/projet222/MainActivity.java
+++ code/app/src/main/java/p222/tp3/projet222/MainActivity.java
@@ -3,12 +3,29 @@ package p222.tp3.projet222;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseHelper dbh = new DatabaseHelper(this.getBaseContext());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.test_upload);
+
+
+        Button bUp = findViewById(R.id.btnUpload);
+        bUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TypeFichier f = new TypeFichier("test", "fake/path");
+                dbh.addFichier(f);
+            }
+        });
     }
+
+
+
 }
--- code/app/src/main/java/p222/tp3/projet222/TypeFichier.java
+++ code/app/src/main/java/p222/tp3/projet222/TypeFichier.java
@@ -1,4 +1,45 @@
 package p222.tp3.projet222;
 
 class TypeFichier {
+
+    private String nom;
+    private String nom_crypt;
+    private String path;
+
+    private boolean encrypted;
+
+    public TypeFichier(String nom, String path) {
+        this.nom = nom;
+        this.path = path;
+        this.encrypted = false;
+    }
+
+    public TypeFichier(String nom, String path, boolean encrypt) {
+        this.nom = nom;
+        this.path = path;
+        this.encrypted = false;
+        if (encrypt) {
+            encryptName();
+        }
+
+    }
+
+    public boolean isEncrypted() {
+        return this.encrypted;
+    }
+
+    public void encryptName() {
+        this.encrypted = true;
+        this.nom_crypt = ""+this.nom.hashCode();
+
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public String getEncName() {
+        return this.nom_crypt;
+    }
+
 }
